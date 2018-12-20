7da4e81e20e6ec1358b666e42c2eac8660d331f0
rajanipos <rajani@digitalrupay.com>
2018-12-20 14:40:52 +0530

    employee design changes

--- src/app/views/employees/employees.component.ts
+++ src/app/views/employees/employees.component.ts
@@ -24,6 +24,7 @@ export function getAlertConfig(): AlertConfig {
   ],
   providers: [{ provide: AlertConfig, useFactory: getAlertConfig }]
 })
+
 export class EmployeesComponent implements OnInit {
   //@ViewChild('myForm') mytemplateForm : ngForm;
   alerts: any[] = [{
@@ -32,8 +33,6 @@ export class EmployeesComponent implements OnInit {
     timeout: 5000
   }];
   totalItems: number;
-  //categorysData: any;
-  //editData: any = [];
   bigCurrentPage: number = 1;
   currentImage: any = '';
   bankuploadedFiles: any;
@@ -45,10 +44,9 @@ export class EmployeesComponent implements OnInit {
   userimagePreview: any;
   userImage: string;
   hideModal = false;
-  //deleteData: { tip_id: any; tip_title: any; tip_description: any; profile_name: any; rec_status: number; };
   alertMessageValue: boolean;
   validBtn: boolean;
-  editData = {
+  employeeData = {
     'emp_firstname': '',
     'emp_lastname': '',
     'emp_password': '',
@@ -57,11 +55,12 @@ export class EmployeesComponent implements OnInit {
     "emp_mobile": "",
     "emp_branch": "hyd",
     "emp_role": "1",
-    "emp_status": "1"
+    "emp_status": "1",
+    "employee_id": null
   }
-  categorysData: any;
+  employeeDetails: any;
   createdValue = false;
-  deleteData: { employee_id: any; emp_firstname: any; emp_address: any; emp_mobile: any; emp_email: any; emp_password: any; emp_branch: any; emp_role: any; emp_status: number; };
+  deleteData: { employee_id: any; emp_status: number; };
   userData: any;
 
   constructor(private spinner: NgxSpinnerService, private service: LoginService, private router: Router, sanitizer: DomSanitizer) {
@@ -76,13 +75,14 @@ export class EmployeesComponent implements OnInit {
     this.service.getEmpList().subscribe(response => {
       this.spinner.hide();
       if (response.json().status == true) {
-        this.categorysData = response.json().data;
-        console.log(this.categorysData);
+        this.employeeDetails = response.json().data;
       } else {
-        this.categorysData = [];
+        this.employeeDetails = [];
       }
-      // this.userData = JSON.parse(localStorage.getItem('loginDetails'));
-      // console.log(this.userData[0].employee_id);
+      if (localStorage.loginDetails) {
+        this.userData = JSON.parse(localStorage.getItem('loginDetails'));
+        console.log(this.userData[0].employee_id);
+      }
     });
   }
 
@@ -102,26 +102,30 @@ export class EmployeesComponent implements OnInit {
   ];
 
   clearData() {
-    this.editData.emp_email = '';
-    this.editData.emp_firstname = '';
-    this.editData.emp_lastname = '';
-    this.editData.emp_mobile = '';
-    this.editData.emp_password = '';
-    this.editData.emp_address = '';
-    this.editData.emp_branch = '';
+    this.employeeData.emp_email = '';
+    this.employeeData.emp_firstname = '';
+    this.employeeData.emp_lastname = '';
+    this.employeeData.emp_mobile = '';
+    this.employeeData.emp_password = '';
+    this.employeeData.emp_address = '';
+    this.employeeData.emp_branch = '';
+    this.employeeData.employee_id = '';
   }
 
-  editPromotion(data, index) {
-    data.index = index;
-    this.editData = data;
-    console.log(this.editData)
+  editEmployee(data, index) {
+    console.log(index)
+    data.index = index
+    console.log(data)
+    this.employeeData = data;
+    console.log(this.employeeData)
   }
+
   clearForm() {
     (<HTMLFormElement>document.getElementById("Login")).reset();
   }
 
   addOrUpdateEmployees(val) {
-    if (this.editData.emp_firstname != '' && this.editData.emp_lastname != '' && this.editData.emp_mobile != '' && this.editData.emp_address != '' && this.editData.emp_password != '' && this.editData.emp_branch != '') {
+    if (this.employeeData.emp_firstname != '' && this.employeeData.emp_lastname != '' && this.employeeData.emp_mobile != '' && this.employeeData.emp_address != '' && this.employeeData.emp_password != '' && this.employeeData.emp_branch != '') {
       let element = document.getElementById("CloseButton");
       console.log(val)
       var data = {
@@ -136,49 +140,41 @@ export class EmployeesComponent implements OnInit {
         emp_role: val.emp_role,
         emp_status: val.emp_status
       }
-      if (!data.employee_id) {
-        this.addCreate();
-        this.clearData();
-      }
+      console.log(data.employee_id)
       this.service.addOrUpdateEmployee(data).subscribe();
-
       element.click();
-      this.categorysData = [];
+      this.employeeDetails = [];
       this.service.getEmpList().subscribe(response => {
-        this.categorysData = response.json().data;
-        console.log(this.categorysData);
+        this.employeeDetails = response.json().data;
+        console.log(this.employeeDetails);
         //this.addCreate();
         this.clearForm();
       });
     }
-
   }
-  DeletePromotion(val) {
+  deleteuser: any = []
+  DeleteEmployee(val, i) {
     console.log(val)
+    console.log(i)
+    this.deleteuser = val;
+    console.log(this.deleteData);
     var data = {
       employee_id: val.employee_id,
-      emp_firstname: val.emp_firstname,
-      emp_address: val.emp_address,
-      emp_mobile: val.emp_mobile,
-      emp_email: val.emp_email,
-      emp_password: val.emp_password,
-      emp_branch: val.emp_branch,
-      emp_role: val.emp_role,
       emp_status: 0
-
     }
     this.deleteData = data;
   }
+
   deleteAlert() {
     this.service.addOrUpdateEmployee(this.deleteData).subscribe();
     this.delete();
-    this.categorysData = [];
+    this.employeeDetails = [];
     this.service.getEmpList().subscribe(response => {
-      this.categorysData = response.json().data;
-      console.log(this.categorysData)
+      this.employeeDetails = response.json().data;
+      console.log(this.employeeDetails)
     });
-
   }
+
   alertsDismiss: any = [];
   add(): void {
     this.alertsDismiss.push({
@@ -187,6 +183,7 @@ export class EmployeesComponent implements OnInit {
       timeout: 5000
     });
   }
+
   addCreate(): void {
     this.alertsDismiss.push({
       type: 'info',
@@ -194,6 +191,7 @@ export class EmployeesComponent implements OnInit {
       timeout: 5000
     });
   }
+
   delete(): void {
     this.alertsDismiss.push({
       type: 'danger',
@@ -201,21 +199,10 @@ export class EmployeesComponent implements OnInit {
       timeout: 5000
     });
   }
-  onSubmit() {
-    console.log(this.editData);
-    this.addOrUpdateEmployees(this.editData)
-    // this.service.rgisterSubmit(this.editData).subscribe(response => {
-    //   this.categorysData = response.json().data;
-    //   console.log(this.categorysData);
-    //   if(this.categorysData.employee_id!=""){
-    //     this.createdValue=true;
-    //   }else{
-    //     this.createdValue=false;
-    //   }
 
-    //this.router.navigate(['dashboard'])
-    // });
-    // this.updatePromotion(this.editData);
+  saveEmployee() {
+    console.log(this.employeeData);
+    this.addOrUpdateEmployees(this.employeeData)
   }
 
 }
