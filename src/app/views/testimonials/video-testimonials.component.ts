171f0b3f07061e2c0cc7beea27e0b36fe56bd787
varunDR <rajani@digitalrupay.com>
2018-12-29 11:12:17 +0530

    beautyTips -incomplete

--- src/app/views/testimonials/video-testimonials.component.ts
+++ src/app/views/testimonials/video-testimonials.component.ts
@@ -2,98 +2,118 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TestmonialsService } from "../../services/TestmonialsService";
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastyService, ToastyConfig, ToastyComponent, ToastOptions, ToastData } from 'ng2-toasty';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: 'video-testimonials.component.html'
 })
 
 export class VideoTestimonialsComponent {
-  categorysData: any;
+
+  toastOptionsSuccess: ToastOptions = {
+    title: "Success",
+    msg: "Successfully Done",
+    showClose: true,
+    timeout: 3000,
+    theme: 'default'
+  };
+  toastOptionsError: ToastOptions = {
+    title: "Error",
+    msg: "Something is Wrong",
+    showClose: true,
+    timeout: 3000,
+    theme: 'default'
+  };
+
+  videoTestimonials: any[];
+  cols: any[];
+  videoTestimonialData: any = {
+    'fullname': '',
+    'description': '',
+    'likes': '',
+    'video': ''
+  }
   editData: any = []
-  deleteData: { testimonial_id: any; description: any; video: any; user_id: any; likes: any; video_thumbnail: any; rec_status: number; };
   userData: any;
-  constructor(private spinner: NgxSpinnerService,private router: Router,private service: TestmonialsService) { }
+  videoTestimonialForm: FormGroup;
+  deleteRecord = '';
+  submitted = false;
+
+  constructor(private spinner: NgxSpinnerService,private router: Router,private service: TestmonialsService,private toastyService: ToastyService, private formBuilder: FormBuilder ) { }
+
   backToDashBoard() {
     this.router.navigate(['reports'])
   }
+
   ngOnInit() {
-    this.spinner.show();
+  this.spinner.show();
   this.service.getVideoTestmonials().subscribe(response => {
-    this.categorysData = response.json().data;
-    console.log(this.categorysData);
+    this.videoTestimonials = response.json().data;
+    console.log(this.videoTestimonials);
     this.userData=JSON.parse(localStorage.getItem('loginDetails'));
     console.log(this.userData[0].employee_id);
     this.spinner.hide();
   });
+
+  this.cols = [
+    { field: 'fullname', header: 'Username' },
+    { field: 'description', header: 'Description' },
+    { field: 'likes', header: 'Likes' },
+    { field: 'video', header: 'Video' }
+  ];
+
+  this.videoTestimonialForm = this.formBuilder.group({
+    Name: ['', Validators.required],
+    Description: ['', Validators.required],
+    Video: ['', Validators.required],
+    Likes: ['', Validators.required],
+  });
 }
-editPromotion(data, index) {
-  data.index = index;
-  this.editData = data;
-  console.log(this.editData)
+
+editVideoTestimonials(data) {
+  this.videoTestimonialData = data;
 }
-openNewWindow(data, index){
-  data.index = index;
-  this.editData = data;
-  console.log(this.editData)
-  var newWindow = window.open(this.editData.video);
-}
-onSubmit() {
-  //console.log(this.editData.tip_title);
-  this.updatePromotion(this.editData);
-}
-updatePromotion(val) {
-  let element = document.getElementById("CloseButton");
-  console.log(val)
+
+updateTestimonial() {
   var data = {
-    description: val.description,
-    video: val.video,
-    user_id: val.user_id,
-    likes: val.likes,
-    testimonial_id: val.testimonial_id,
-    video_thumbnail: val.video_thumbnail,
-    updatedempid:this.userData[0].employee_id
+    testimonial_id: this.videoTestimonialData.testimonial_id,
+    description: this.videoTestimonialData.description,
+    likes: this.videoTestimonialData.likes,
+    video: this.videoTestimonialData.video,
+    rec_status: this.videoTestimonialData.rec_status,
+    updatedempid: this.userData[0].employee_id
   }
-  console.log(data);
-  this.service.editVideoTestmonials(data).subscribe();
-  this.add();
-  element.click();
-}
-DeletePromotion(val) {
-  console.log(val)
-  var data = {
-    testimonial_id: val.testimonial_id,
-    description: val.description,
-    video: val.video,
-    user_id: val.user_id,
-    likes: val.likes,
-    video_thumbnail: val.video_thumbnail,
-    rec_status: 0
-  }
- 
-  this.deleteData=data;
-}
-deleteAlert(){
-  this.service.editVideoTestmonials(this.deleteData).subscribe();
-  this.delete();
-  this.categorysData=[];
-  this.service.getVideoTestmonials().subscribe(response => {
-    this.categorysData = response.json().data;
-    console.log(this.categorysData)
+  let modelClose = document.getElementById("CloseButton");
+  this.service.editVideoTestmonials(data).subscribe(res => {
+    modelClose.click();
+    if (res.json().status == true) {
+      this.toastyService.success(this.toastOptionsSuccess);
+      this.videoTestimonialData = data
+    }else{
+      this.toastyService.error(this.toastOptionsError);
+    }
   });
 }
-alertsDismiss: any = [];
-add(): void {
-  this.alertsDismiss.push({
-    type: 'info',
-    msg: `Updated Sucessfully!`,
-    timeout: 5000
+
+deleteVideoTestimonial(val,index) {
+  this.deleteRecord = val
+  console.log(this.deleteRecord)
+  this.deleteRecord["index"] = index
+  console.log( this.deleteRecord["index"])
+}
+
+deleteAlert() {
+  console.log(this.deleteRecord["testimonial_id"])
+  this.service.editVideoTestmonials({testimonial_id: this.deleteRecord["testimonial_id"],rec_status: 0}).subscribe(res =>{
+    console.log(res.json())
+    if (res.json().status == true) {
+      this.videoTestimonials.splice(this.deleteRecord["index"], 1)
+      this.toastyService.success(this.toastOptionsSuccess);
+    }else{
+      this.toastyService.error(this.toastOptionsError);
+    }
   });
 }
-delete(): void {
-  this.alertsDismiss.push({
-    type: 'danger',
-    msg: `Deleted Sucessfully!`,
-    timeout: 5000
-  });
-}
+
 }
