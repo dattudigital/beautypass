69b80fe8d0be6f2d12553e51bd9cd80cd2227f38
dattudigital <dattu@digitalrupay.com>
2019-01-21 20:41:38 +0530

    user history search

--- src/app/views/refferal-rewards/user-history.component.ts
+++ src/app/views/refferal-rewards/user-history.component.ts
@@ -20,27 +20,50 @@ export class UserHistoryComponent {
   cols: any = [];
   temp: any[] = new Array();
 
-  constructor(private spinner: NgxSpinnerService,private dp: DatePipe,private service: RefferalRewardsService) { }
+  constructor(private spinner: NgxSpinnerService, private dp: DatePipe, private service: RefferalRewardsService) { }
 
   ngOnInit() {
     this.cols = [
       { field: 'user_id', header: 'User ID' },
       { field: 'fullname', header: 'Name' },
       { field: 'email_id', header: 'Email' },
-       { field: 'dob', header: 'DOB' ,type: this.dp},
+      { field: 'dob', header: 'DOB', type: this.dp },
       { field: 'location', header: 'Location Id' },
       { field: 'locationName', header: 'Location Name' },
       { field: 'studioid', header: 'Studio Id' },
       { field: 'studioName', header: 'StudioName' },
+      { field: 'reward_for', header: 'Reward For' },
+      { field: 'points', header: 'Points' },
+      { field: 'debit', header: 'Debit' },
+      { field: 'rewarddate', header: 'Purchase Date', type: this.dp }
     ];
 
   }
   onSelect(event: TypeaheadMatch): void {
-    var data = [];
-    data.push(event.item)
-    this.selectedOption = data;
-    console.log(this.selectedOption)
-    this.tableStatus = true;
+    // var data = [];
+    // data.push(event.item)
+    // this.selectedOption = data;
+    // console.log(this.selectedOption)
+    // this.tableStatus = true;
+    console.log(event.item)
+    console.log(event.item["studioid"])
+    let URL = '';
+    if (event.item["mindbody_id"]) {
+      URL = URL + '/' + event.item["mindbody_id"]
+    }
+    if (event.item["studioid"]) {
+      URL = URL + '/' + event.item["studioid"]
+    }
+    this.spinner.show();
+    this.service.getUserHistory(URL).subscribe(res => {
+      this.spinner.hide();
+      if (res.json().status == true) {
+        this.tableStatus = true;
+        this.selectedOption = res.json().data;
+      }
+    }, (err) => {
+      this.spinner.hide();
+    })
   }
 
   userSearch(val) {
